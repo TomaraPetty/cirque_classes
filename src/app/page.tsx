99bc2a2144377633dd9634e1bf4445f9ae7f2467
Tomara Petty <tomararuth@gmail.com>
2024-03-15 16:43:32 -0700

    add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen items-center justify-center'>
       <div className='flex flex-col gap-2 p-5 max-w-xs bg-slate-800'>
+        <Image
+          src='/logo_white.png'
+          alt='Cirque Physio Logo'
+          className='dark:invert'
+          width={100}
+          height={24}
+          priority
+        />
         <div className='text-center my-4 text-white'>
           <h1>Cirque Classes</h1>
         </div>
