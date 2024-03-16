41978f008a8da6423c145c2ea831224043eda6f6
Tomara Petty <tomararuth@gmail.com>
2024-03-15 18:03:07 -0700

    add styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,48 +4,50 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className='flex min-h-screen items-center justify-center'>
-      <div className='flex flex-col gap-2 p-5 max-w-xs bg-slate-800'>
-        <Image
-          src='/logo_white.png'
-          alt='Cirque Physio Logo'
-          className='dark:invert'
-          width={100}
-          height={24}
-          priority
-        />
-        <div className='text-center my-4 text-white'>
-          <h1>Cirque Classes</h1>
+      <div className='flex flex-col gap-2 p-5 max-w-xs w-full rounded-lg bg-[#210070]'>
+        <div className='flex flex-col items-center justify-center'>
+          <Image
+            src='/logo_white.png'
+            alt='Cirque Physio Logo'
+            className='dark:invert'
+            width={150}
+            height={50}
+            priority
+          />
+        </div>
+        <div className='text-center text-xl font-bold italic tracking-wide my-4 text-[#2ee2ff]'>
+          <h1>WEEKLY SCHEDULE</h1>
         </div>
         <div>
           <Link
             href='/'
-            className='text-white bg-slate-900 my-4 p-3 rounded-lg block'
+            className='text-[#210070] font-bold tracking-wide bg-[#2ee2ff] my-4 p-3 rounded-lg block'
           >
-            Monday
+            Monday:
           </Link>
         </div>
         <div>
           <Link
             href='/'
-            className='text-white bg-slate-900 my-4 p-3 rounded-lg block'
+            className='text-[#210070] font-bold tracking-wide bg-[#2ee2ff] my-4 p-3 rounded-lg block'
           >
-            Tuesday
+            Tuesday:
           </Link>
         </div>
         <div>
           <Link
             href='/'
-            className='text-white bg-slate-900 my-4 p-3 rounded-lg block'
+            className='text-[#210070] font-bold tracking-wide bg-[#2ee2ff] my-4 p-3 rounded-lg block'
           >
-            Wednesday
+            Wednesday:
           </Link>
         </div>
         <div>
           <Link
             href='/'
-            className='text-white bg-slate-900 my-4 p-3 rounded-lg block'
+            className='text-[#210070] font-bold tracking-wide bg-[#2ee2ff] my-4 p-3 rounded-lg block'
           >
-            Thursday
+            Thursday:
           </Link>
         </div>
       </div>
